9b82091f21a29aec9730b831a6e4d1415e81d705
ldemel <ldemel@hotmail.com>
2016-09-16 10:47:08 -0500

    Add files via upload

---
+++ leogo.py
@@ -0,0 +1,11 @@
+def sing_song():
+    print("i had a friend who had a friend, and leogo was his name-o")
+    print("L E O G O")
+    print("L E O G O")
+    print("L E O G O")
+    print("and LEOGO was his name-o\n")
+
+sing_song()
+sing_song()
+sing_song()
+sing_song()
